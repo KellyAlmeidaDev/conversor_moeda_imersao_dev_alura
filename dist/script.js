2558b9c162c2d44e26520e2f2614395e3c0dd0b1
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2025-12-10 08:55:14 -0300

    Primeira versão

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+function conversor(){
+    valorDolar = prompt('Digite um valor em Dólar')
+    umDolar = 5.71
+    alert(`O valor em reais é R$ ${umDolar * valorDolar}. Mas lembre! O crime não vele a pena!!`)
+}
